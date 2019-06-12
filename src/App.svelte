553f50c41d25a2e1072c28b93d1363ad9e578ddd
michaeljamie <mike@noke.com>
2019-06-12 15:35:31 -0600

    added search component and links

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Fetch from "./Fetch.svelte";
+  import Form from "./Form.svelte";
+
+  let searchTerm;
+
+  function handleSubmit() {
+	  const { value } = this.elements.search;
+	  searchTerm = value;
+  }
+</script>
+
+<Form {handleSubmit} />
+<Fetch {searchTerm} let:data>
+  <h1>A list of links</h1>
+  <ul>
+    {#each data as link}
+      <li>
+        <a href={link.url}>{link.title}</a>
+      </li>
+    {/each}
+  </ul>
+</Fetch>
+
